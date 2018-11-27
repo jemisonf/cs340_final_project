8c05594dcac551a4129c82c013e565a22b0f32d5
Fischer Jemison <jemisonf@oregonstate.edu>
2018-11-26 20:36:56 -0800

    wip

--- content/js/app/App.jsx
+++ content/js/app/App.jsx
@@ -7,7 +7,7 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentUser: {},
+            currentUser: 21,
             bearerToken: "abcdefg",
         }
     }
